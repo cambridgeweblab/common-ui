0c92ccd553b0ce09e40081e3e007e500c9ac06b7
Noris Alsaadie <noris@bluemagesoftware.com>
2016-10-06 15:50:02 +0100

    CA Dialog web component (#1)
    * CA Dialog web component
    
    This commit adds ca-dialog and the testing framework in.
    
    /components - this is where all the JS and CSS lives, including helpers.
    
    /examples - example pages live here, npm start then localhost:8080
    
    /test - where all the tests live. The are .html files
    
    * Adding tests for helpers migrated from old project
    
    /test/helpers - test cases for helpers
    /components/helpers/utl.js renamed to /components/helpers/types.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+const server = express();
+
+server
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(compression())
+    .set('view engine', 'html')
+    .set('views', path.resolve(__dirname, 'examples'))
+    .use('/bower_components', express.static(path.join(__dirname, 'bower_components')))
+    .use('/examples', express.static(path.join(__dirname, 'examples')))
+    .use('/components', express.static(path.join(__dirname, 'components')))
+    .get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'examples', 'index.html'));
+    })
+    .get('/:component', (req, res) => {
+        const component = req.params.component;
+        res.sendFile(path.resolve(__dirname, 'examples', `${component}.html`));
+    })
+    .listen(PORT, () => {
+        console.log(`server up and listening on ${PORT}`);
+    });
