08584df92aec2d8568261d105fd46d88a6391745
kmarchant <km@mitsol.co.uk>
2016-10-07 19:43:11 +0100

    WebComponents: initial check-in for ca-email-confirm (most tests still to be written)

--- components/helpers/types.js
+++ components/helpers/types.js
@@ -1,4 +1,5 @@
 define('types', [], () => {
+
     /**
     * toBoolean - casts a string value to boolean.
     * @param {string} value - value to cast to boolean.
@@ -8,7 +9,18 @@ define('types', [], () => {
         return value === 'true';
     }
 
+    /**
+     * isValidEmailAddress - returns if email passes regex validation
+     * @param {string} emailAddress - email address string to validate
+     * @return {boolean} email address is valid
+     */
+    function isValidEmailAddress(emailAddress) {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(emailAddress);
+    }
+
     return {
-        toBoolean
+        toBoolean,
+        isValidEmailAddress
     };
 });
