9e53a9106c462157a6516640ef90f4237afb8ef6
Noris Alsaadie <noris@bluemagesoftware.com>
2016-12-16 11:06:52 +0000

    CA DATE TIME (#12)
    * CA DATE TIME
    
    Migrated ca-datetime from the old repo. Had to also migrate component support.
    
    bower.json -> adding pikaday
    components/ca-datetime.css -> migrated
    components/ca-datetime.js -> top and tail migrate.
    components/config/index.js -> ading pikaday
    components/helpers/component-support.js -> top and tail migrate.
    components/helpers/is-mobile.js -> migrated from helper.js.
    components/helpers/secureajax.js -> taken from big-red-button branch
    examples/ca-datetime.html -> date time example
    test/ca-datetime-test.html -> placeholder for tests
    
    * Renaming DateTime class to DateTimeInput
    
    * Fixing issue with secure ajax

--- components/config/index.js
+++ components/config/index.js
@@ -1,7 +1,8 @@
 /* global SystemJS */
 SystemJS.config({
     map: {
-        'document-register-element': '/bower_components/document-register-element/build/document-register-element.amd.js'
+        'document-register-element': '/bower_components/document-register-element/build/document-register-element.amd.js',
+        pikaday: '/bower_components/pikaday/pikaday.js'
     },
     baseURL: '/components'
 });
