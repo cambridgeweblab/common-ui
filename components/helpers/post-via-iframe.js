479cb47e665787b861ea4f2975e14ccd5801ef1d
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2017-04-25 23:33:52 +0100

    Migrating ca-import (#34)
    * Initial migration
    
    * Migrating ca-import
    
    * PR feedback
    
    * Feedback on .call

--- components/helpers/post-via-iframe.js
+++ components/helpers/post-via-iframe.js
@@ -1,12 +1,12 @@
-define('post-via-iframe', ['./create-element.js'], (createElement) =>
+define('post-via-iframe', ['./create-element.js'], createElement =>
     /**
       * Sends data to the server via a form post in a hidden frame
       * @param {string} url - url to post the data to
       * @param {object} data - JSON key/value object - a hidden field is created for each key
       */
     function postViaIframe(url, data) {
+  
         const targetName = `_post_via_iframe_${Date.now()}`;
-        // eslint-disable-next-line no-used-vars
         const iframe = createElement(document.body, 'iframe', {
             src: '',
             name: targetName,
@@ -19,17 +19,15 @@ define('post-via-iframe', ['./create-element.js'], (createElement) =>
             target: targetName,
             style: 'display: none !important'
         });
-
-        // eslint-disable-next-line no-restricted-syntax
-        for (const key in data) {
-            if (Object.prototype.hasOwnProperty.call(data, key)) {
-                createElement(form, 'input', {
-                    type: 'hidden',
-                    name: key,
-                    value: data[key]
-                });
-            }
-        }
+  
+        Object.keys(data).forEach(key => {
+          
+            createElement(form, 'input', {
+                type: 'hidden',
+                name: key,
+                value: data[key]
+            });
+        });
 
         form.submit();
     }
