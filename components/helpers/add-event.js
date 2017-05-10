ab97140eaff7b8258d333bd7e1662315c3c891b8
tapina <github@tapina.com>
2017-05-10 08:48:31 +0100

    HTML imports (#42)
    * Linting *.html files to get ready for html imports
    
    * Fix some linting errors.
    
    * Convert all elements and helpers to IMD-ready HTML imports.
    
    Examples and tests still to migrate.
    
    * Fix module name of remove-class
    
    * Fix vestigial prototype hack
    
    * Convert examples to work with HTML imports.
    
    * Convert tests to work with HTML imports. A11y tests now fail!
    
    * Fixing test cases for helpers and dep bump
    
    * Fixed aria issue with ca-barcode
    
    * Fixed aria issue with ca-confirm-input
    
    * Fixed aria issue with ca-currency
    
    * Fixed aria issue with ca-datetime
    
    * Fixed aria issue with ca-file-input
    
    * Fixed aria issue with ca-image
    
    * accessbility tests now passing

--- components/helpers/add-event.js
+++
@@ -1,9 +0,0 @@
-define('add-event', [], () =>
-    function (el, type, func) {
-        if (el.addEventListener) {
-            el.addEventListener(type, func, false);
-        } else if (el.attachEvent) {
-            el.attachEvent(`on${type}`, func);
-        }
-    }
-);
--- components/helpers/remove-class.js
+++
@@ -1,9 +0,0 @@
-define('add-class', ['./has-class.js'], (hasClass) =>
-    function (el, cls) {
-        if (hasClass(el, cls)) {
-            const reg = new RegExp(`\\b${cls}\\b`);
-            // eslint-disable-next-line no-param-reassign
-            el.className = el.className.replace(reg, '').replace('  ', ' ');
-        }
-    }
-);
